bf0b683cbb430835d54dc272a5900596b2a08c54
duncan <iamduncan@users.noreply.github.com>
2022-01-31 19:47:34 +0000

    :bug: markdown formatter updated

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,24 +1,13 @@
-import { unified } from 'unified';
+import remark from 'remark';
 import html from 'remark-html';
-import remarkParse from 'remark-parse';
-import remarkRehype from 'remark-rehype';
-import rehypeDocument from 'rehype-document';
-import rehypeFormat from 'rehype-format';
-import rehypeSlug from 'rehype-slug';
-import rehypeStringify from 'rehype-stringify';
-import prism from 'remark-prism';
+import remarkPrism from 'remark-prism';
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await unified()
-    .use(remarkParse)
-    .use(prism, {
+  const result = await remark()
+    .use(html)
+    .use(remarkPrism, {
       plugins: ['line-numbers', 'treeview'],
     })
-    .use(remarkRehype)
-    .use(rehypeSlug)
-    .use(rehypeDocument)
-    .use(rehypeFormat)
-    .use(rehypeStringify)
     .process(markdown);
   return result.toString();
 }
