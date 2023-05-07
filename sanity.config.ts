6ecc8b3b1722eccddf87f38fa661971247dd7413
Duncan Brown <iamduncan@users.noreply.github.com>
2023-05-07 08:47:36 +0100

    :arrow_up: yarn.lock rebuilt, conosle.log removed

--- sanity.config.ts
+++ sanity.config.ts
@@ -10,7 +10,6 @@ import { table } from '@sanity/table';
 import { codeInput } from '@sanity/code-input';
 
 export const PREVIEWABLE_DOCUMENT_TYPES: string[] = ['post'];
-console.log(projectId);
 
 export default defineConfig({
   name: 'default',
