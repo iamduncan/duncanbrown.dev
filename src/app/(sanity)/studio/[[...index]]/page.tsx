837bb93451801a60f55e2d73e40af3e527b1f326
Duncan Brown <iamduncan@users.noreply.github.com>
2023-08-05 14:32:11 +0100

    error boundary with sentry logging added to sanity studio

--- src/app/(sanity)/studio/[[...index]]/page.tsx
+++ src/app/(sanity)/studio/[[...index]]/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 /**
  * This route is responsible for the built-in authoring environment using Sanity Studio v3.
@@ -9,9 +9,37 @@
  * https://github.com/sanity-io/next-sanity
  */
 
-import { NextStudio } from "next-sanity/studio";
-import config from "sanity.config";
+import { NextStudio } from 'next-sanity/studio';
+import config from 'sanity.config';
+import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
+import * as Sentry from '@sentry/nextjs';
+
+function Fallback({ error, resetErrorBoundary }: FallbackProps) {
+  // Call resetErrorBoundary() to reset the error boundary and retry the render.
+
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{ color: 'red' }}>{error.message}</pre>
+    </div>
+  );
+}
+
+const logError = (error: Error, info: { componentStack: string }) => {
+  // Do something with the error, e.g. log to an external API
+  Sentry.captureException(error);
+};
 
 export default function StudioPage() {
-  return <NextStudio config={config} />;
+  return (
+    <ErrorBoundary
+      FallbackComponent={Fallback}
+      onReset={(details) => {
+        // Reset the state of your app so the error doesn't happen again
+      }}
+      onError={logError}
+    >
+      <NextStudio config={config} />
+    </ErrorBoundary>
+  );
 }
